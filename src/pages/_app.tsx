2a46070cc73da68521866af87dedc98b09958961
Hyeungshik Jung <hyungshike@gmail.com>
2020-03-21 21:58:07 +0900

    chore: Add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
-import "bulma/css/bulma.css";
+import Head from "next/head";
 import Router from "next/router";
 
 import * as gtag from "../lib/gtag";
 
+import "bulma/css/bulma.css";
+
 Router.events.on("routeChangeComplete", url => gtag.pageview(url));
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>ABC마트 사이즈별 저렴이 줍줍</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
